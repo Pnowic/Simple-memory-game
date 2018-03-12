06cfe251eca2eb4d8e3c2ae05a6bbccee3d1c4b5
Piotr Nowicki <nowickipiotrus@gmail.com>
2018-03-12 22:12:07 +0100

    create main constructor, working upon game start method add main styles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,57 @@
 import "../scss/style.scss"
+
+const memoryGame = {
+    tileCount: 20, //number of tiles
+    tileOnRow: 5, //tiles in row
+    divBoard: null, //board
+    divScore: null, //result
+    tiles: [], //shuffled tiles
+    tilesChecked: [], //checked tiles
+    moveCount: 0, //number of moves
+    tilesImg: [ //tiles images
+        'images/tile1.png',
+        'images/tile2.png',
+        'images/tile3.png',
+        'images/tile4.png',
+        'images/tile5.png',
+        'images/tile6.png',
+        'images/tile7.png',
+        'images/tile8.png',
+        'images/tile9.png',
+        'images/tile10.png'
+    ],
+
+
+    this: startGame = function () {
+
+        for (let i = 0; i < tileCount; i++) {
+            this.tiles.push(Math.floor(i / 2));
+        }
+
+        for (let i = this.tileCount - 1; i > 0; i--) {
+            const swap = Math.floor(Math.random() * i);
+            const tmp = this.tiles[i];
+            this.tiles[i] = this.tiles[swap];
+            this.tiles[swap] = tmp;
+        }
+
+        for (let i = 0; i < this.tileCount; i++) {
+            const tile = document.createElement('div');
+            tile.classList.add("game-tile");
+            this.divBoard.appendChild(tile);
+
+            tile.dataset.cardType = this.tiles[i];
+            tile.dataset.index = i;
+
+            tile.addEventListener('click', this.tileClick.bind(this));
+        }
+
+
+    },
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.game-start').addEventListener('click', function() {
+        memoryGame.startGame();
+    });
+});
--- js/out.js
+++ js/out.js
@@ -72,6 +72,49 @@
 
 __webpack_require__(1);
 
+var memoryGame = {
+    tileCount: 20, //number of tiles
+    tileOnRow: 5, //tiles in row
+    divBoard: null, //board
+    divScore: null, //result
+    tiles: [], //shuffled tiles
+    tilesChecked: [], //checked tiles
+    moveCount: 0, //number of moves
+    tilesImg: [//tiles images
+    'images/tile1.png', 'images/tile2.png', 'images/tile3.png', 'images/tile4.png', 'images/tile5.png', 'images/tile6.png', 'images/tile7.png', 'images/tile8.png', 'images/tile9.png', 'images/tile10.png'],
+
+    this: startGame = function startGame() {
+
+        for (var i = 0; i < tileCount; i++) {
+            this.tiles.push(Math.floor(i / 2));
+        }
+
+        for (var _i = this.tileCount - 1; _i > 0; _i--) {
+            var swap = Math.floor(Math.random() * _i);
+            var tmp = this.tiles[_i];
+            this.tiles[_i] = this.tiles[swap];
+            this.tiles[swap] = tmp;
+        }
+
+        for (var _i2 = 0; _i2 < this.tileCount; _i2++) {
+            var tile = document.createElement('div');
+            tile.classList.add("game-tile");
+            this.divBoard.appendChild(tile);
+
+            tile.dataset.cardType = this.tiles[_i2];
+            tile.dataset.index = _i2;
+
+            tile.addEventListener('click', this.tileClick.bind(this));
+        }
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.game-start').addEventListener('click', function () {
+        memoryGame.startGame();
+    });
+});
+
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
@@ -131,7 +174,7 @@ exports = module.exports = __webpack_require__(3)(false);
 
 
 // module
-exports.push([module.i, "", ""]);
+exports.push([module.i, "* {\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box; }\n\n.game-board {\n  margin: 30px auto;\n  position: relative;\n  background: lightblue;\n  width: 600px;\n  height: 500px;\n  border: 2px solid lightcyan; }\n\n.game-tile {\n  position: absolute;\n  width: 100px;\n  height: 100px;\n  background: #f6da17; }\n", ""]);
 
 // exports
 
